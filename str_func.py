04d9bcb17023edfae54e9f38ae04ad05d244be8a
diana <diana-safina-2017@mail.ru>
2023-09-14 15:08:52 +0500

    вторая функция

--- str_func.py
+++ str_func.py
@@ -2,3 +2,12 @@
 def chage(string):
     return string.upper()
 
+def new_function(word):
+ """ функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции."""
+ return word.title()
+
+
+
+
+
+
